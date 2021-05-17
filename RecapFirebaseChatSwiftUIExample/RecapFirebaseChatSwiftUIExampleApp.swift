f6adea916c7ba75a1c3c7eed9a1a8a111e3eb0c7
Fumiya Sakai <just1factory@gmail.com>
2021-05-17 13:04:17 +0900

    新しいProjectに移管する

---
+++ RecapFirebaseChatSwiftUIExample/RecapFirebaseChatSwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecapFirebaseChatSwiftUIExampleApp.swift
+//  RecapFirebaseChatSwiftUIExample
+//
+//  Created by 酒井文也 on 2021/05/17.
+//
+
+import SwiftUI
+
+@main
+struct RecapFirebaseChatSwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
